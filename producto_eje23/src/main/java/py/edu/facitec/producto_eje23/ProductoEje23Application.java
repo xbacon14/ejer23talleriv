b855ea82cb6779f0e0eb3199e117615c73f61d79
xbacon14 <italo.galeano123@gmail.com>
2020-03-15 13:45:57 -0300

    empezando tarea

---
+++ producto_eje23/src/main/java/py/edu/facitec/producto_eje23/ProductoEje23Application.java
@@ -0,0 +1,13 @@
+package py.edu.facitec.producto_eje23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoEje23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoEje23Application.class, args);
+	}
+
+}
---
+++ producto_eje23/src/test/java/py/edu/facitec/producto_eje23/ProductoEje23ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.facitec.producto_eje23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoEje23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
