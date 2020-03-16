18098654b029ae7e503b8ce7efdedfd1ef41bcc1
xbacon14 <italo.galeano123@gmail.com>
2020-03-16 14:08:18 -0300

    trabajo ok

--- producto_eje23/src/main/java/py/edu/facitec/producto_eje23/controller/InicioController.java
+++ producto_eje23/src/main/java/py/edu/facitec/producto_eje23/controller/InicioController.java
@@ -1,6 +1,7 @@
 package py.edu.facitec.producto_eje23.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,18 +25,18 @@ public class InicioController {
 	@Autowired
 	private ProductoRepository productoRepository;
 
-	@RequestMapping(value = "/producto/eliminar/{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/eliminar/{id}", method = RequestMethod.DELETE)
 	public void elimProducto(@PathVariable Long id) {
-		productoRepository.deleteInBatch(id);
+		productoRepository.deleteById(id);
 	}
 
-	@RequestMapping(value = "/producto/lista", method = RequestMethod.GET)
-	public ResponseEntity<List<Producto>> findByNombre(@RequestParam(name = "descripcion") String descripcion) {
-		List<Producto> list = productoRepository.findByNombre(descripcion);
+	@RequestMapping(value = "/lista", method = RequestMethod.GET)
+	public ResponseEntity<Optional<Producto>> findByAll(@RequestParam(name = "descripcion") Long id) {
+		Optional<Producto> list = productoRepository.findById(id);
 		return ResponseEntity.ok(list);
 	}
 
-	@RequestMapping("/producto/guardar")
+	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
 	public ResponseEntity<Producto> save(@RequestBody Producto producto) {
 		producto = productoRepository.save(producto);
 		return ResponseEntity.ok(producto);
--- producto_eje23/src/main/java/py/edu/facitec/producto_eje23/repo/ProductoRepository.java
+++ producto_eje23/src/main/java/py/edu/facitec/producto_eje23/repo/ProductoRepository.java
@@ -1,6 +1,7 @@
 package py.edu.facitec.producto_eje23.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,6 @@ import py.edu.facitec.producto_eje23.entity.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
-	List<Producto> findByNombre(String producto);
+	Optional<Producto> findById(Long id);
 
 }
